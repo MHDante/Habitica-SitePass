d1ac61f1b539fe8e59b7039d369306eee3734c99
MHDante <mhdcj@hotmail.com>
2015-01-19 11:44:36 -0500

    Refactor. Currently no saving.

--- Penalizer/app/background.js
+++ Penalizer/app/background.js
@@ -1,99 +1,104 @@
-var Domains = [];
-var Credentials = [];
-var Monies = 0;
-var PurchaseRewardTask = 
+"use strict";
+var Consts = {
+    serverUrl : 'https://habitrpg.com:443/api/v2',
+    serverPathUser : '/user',
+    serverPathTasks : '/user/tasks',
+    RewardTemplate :
         {
             text: "SitePass",
             value: 0,
-            notes: "Reward utilized by HabitRPG SitePass. Changes value depending on last accessed website.",
+            notes:  "Reward utilized by HabitRPG SitePass."+
+                    " Changes value depending on last accessed website.",
             id: "sitepass",
             type: "reward"
-        };
-var UserObj;
+        },
+    urlFilter: { urls: ["<all_urls>"], types: ["main_frame"] },
+    opt_extraInfoSpec: ["blocking"],
+    userDataKey : "USER_DATA"
+};
 
-var accessDurationMins = 10;
-var serverUrl = 'https://habitrpg.com:443/api/v2';
-var serverPathUser = '/user';
-var serverPathTasks = '/user/tasks';
+var Vars = {
+    RewardTask: Consts.RewardTemplate,
+    Monies: 0,
+    UserData: new UserSettings()
+};
 
-function Domain(hostname, cost, passExpiry) {
+function UserSettings() {
+    this.BlockedSites = {};
+    this.Credentials = {uid:"",apiToken:""};
+    this.PassDurationMins = 10;
+    this.GetBlockedSite = function (hostname) {
+        return this.BlockedSites[hostname];
+    }
+    this.RemoveBlockedSite = function (site) {
+        if (site.hostname) {
+            delete this.BlockedSites[site.hostname];
+        }else
+            delete this.BlockedSites[site];
+    };
+    this.AddBlockedSite = function (hostname, cost, passExpiry) {
+        this.BlockedSites[hostname] = new BlockedSite(hostname, cost, passExpiry);
+        return this.BlockedSites[hostname];
+    }
+
+}
+
+function BlockedSite(hostname, cost, passExpiry) {
     this.hostname = hostname;
     this.cost = cost;
     this.passExpiry = passExpiry;
+    this.isAllowed = function() { return this.passExpiry > Date.now() };
 }
 
-var callback = function (details) {
-    //alert(details.url);
-    var hostname = new URL(details.url).hostname;
-    var index = -1;
-    for (var j = 0; j < Domains.length; j++) {
-        if (Domains[j].hostname == hostname) {
-            index = j;
-            break;
-        }
-    }
+var callback = function(details) {
+    var site = Vars.UserData.GetBlockedSite(new URL(details.url).hostname);
+    if (!site || site.isAllowed) return { cancel: false };
 
-    if (index > -1) {
-        var domain = Domains[index];
-        if (domain.passExpiry > Date.now()) {
-            return { cancel: false };
-        }
-        if (domain.cost > Monies) {
-            alert("You can't afford it!\nComplete some tasks and try again!");
-            return { redirectUrl: "http://google.com/" };
-        }
-        var r = confirm("You're trying to Access " + domain.hostname + "!\nThis Will cost you " + domain.cost.toFixed(2) + " to access for " + accessDurationMins + " minutes!");
-        if (r == true) {
-            domain.passExpiry = Date.now() + accessDurationMins * 60 * 1000;
-            return { cancel: false };
-        } else {
-            return { redirectUrl: "http://google.com/" };
-        }
-    };
-    return { cancel: false };
+    if (site.cost > Monies) {
+        alert(  "You can't afford to visit " + site.hostname + " !\n" +
+                "Complete some tasks and try again!");
+        return { redirectUrl: "http://google.com/" };
+    }
+    var r = confirm("You're trying to Access " + site.hostname + "!\n" +
+        "It will cost you " + site.cost.toFixed(2) +
+        " to access for " + Vars.UserData.PassDurationMins + " minutes!");
+    if (r) {
+        ConfirmPurchase(site);
+        site.passExpiry = Date.now() + Vars.UserData.PassDurationMins * 60 * 1000;
+        return { cancel: false };
+    } else {
+        return { redirectUrl: "http://google.com/" };
+    }
 };
 
-var filter = {urls: ["<all_urls>"] , types: ["main_frame"] };
+chrome.webRequest.onBeforeRequest.addListener(callback, Consts.urlFilter, Consts.opt_extraInfoSpec);
 
-var opt_extraInfoSpec = ["blocking"];
-chrome.webRequest.onBeforeRequest.addListener(
-        callback, filter, opt_extraInfoSpec);
-
-chrome.storage.sync.get("save", function (result) {
-    console.log(typeof result.save);
-    if (!(result.save instanceof Array)) {
-        Domains = [];
-    } else {
-        Domains = result.save;
+chrome.storage.sync.get(Consts.userDataKey, function (result) { 
+    if (result.Credentials) {
+        Vars.UserData = result[Consts.userDataKey];
+        FetchHabitRPGData();
     }
 });
 
-chrome.storage.sync.get("creds", function (result) {
-    
-    console.log(typeof result.creds);
-    if (!(result.creds instanceof Array)) {
-        Credentials = ["", ""];
-    } else {
-        Credentials = result.creds;
-    }
-    FetchUserData();
-});
+function FetchHabitRPGData() {
 
+    if (!Credentials||
+        Credentials.uid == "" ||
+        Credentials.apiToken == "") return;
 
-function FetchUserData() {
-    if (Credentials.length < 1 || Credentials[0] == "" || Credentials[1] == "")return;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", serverUrl + serverPathUser, false);
     xhr.setRequestHeader("x-api-user", Credentials[0]);
     xhr.setRequestHeader("x-api-key", Credentials[1]);
     xhr.send();
-    UserObj = JSON.parse(xhr.responseText);
-    Monies = UserObj.stats.gp;
-    var rewards = UserObj.rewards;
-    if (rewards && rewards.constructor === Array) { //this might be replacable by one api call.
+    var userObj = JSON.parse(xhr.responseText);
+    Vars.Monies = userObj["stats"]["gp"];
+    //This might be replacable by one api call. But it seems wasteful to query the server again.
+    var rewards = userObj["rewards"];
+    if (rewards && rewards.constructor === Array) { 
         for (var i = 0; i < rewards.length; i++) {
-            if (rewards[i] && rewards[i].id == "SitePass") {
-                PurchaseRewardTask == rewards[i];
+            if (rewards[i] && rewards[i].id == "sitepass") {
+                Vars.RewardTask = rewards[i];
                 return;
             }
         }
@@ -104,7 +109,7 @@ function FetchUserData() {
 
 function UpdateTask(cost, create) {
     
-    PurchaseRewardTask.cost = cost;
+    Vars.RewardTask.cost = cost;
     var xhr2 = new XMLHttpRequest();
     if (create) {
         xhr2.open("POST", serverUrl + serverPathTasks, false);
@@ -114,13 +119,13 @@ function UpdateTask(cost, create) {
     xhr2.setRequestHeader("Content-Type", "application/json");
     xhr2.setRequestHeader("x-api-user", Credentials[0]);
     xhr2.setRequestHeader("x-api-key", Credentials[1]);
-    xhr2.send(JSON.stringify(PurchaseRewardTask));
-    PurchaseRewardTask = JSON.parse(xhr2.responseText);
+    xhr2.send(JSON.stringify(Vars.RewardTask));
+    Vars.RewardTask = JSON.parse(xhr2.responseText);
 }
 
 
 
-function ConfirmPurchase(domain) {
+function ConfirmPurchase(site) {
 
 
 }
--- Penalizer/app/popup.js
+++ Penalizer/app/popup.js
@@ -1,5 +1,151 @@
-var background = chrome.extension.getBackgroundPage();
+Node.prototype.AppendText = function (string) { this.appendChild(document.createTextNode(string)); }
+Node.prototype.AppendBreak = function () { this.appendChild(document.createElement("br")); }
 
+
+var background = chrome.extension.getBackgroundPage();
+var Vars = background.Vars;
+var Consts = background.Consts;
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    getCurrentTabUrl(function(url) {
+        GenerateBlockCommand(url);
+        document.body.AppendBreak();
+        var para = document.createElement("p");
+        para.appendChild(document.createTextNode("Blocked Sites:"));
+        document.body.appendChild(para);
+
+        var table = document.createElement("table");
+        var blockedSites = Vars.UserData.BlockedSites;
+        for (var site in blockedSites) {
+            if (blockedSites.hasOwnProperty(site)) {
+                var row = document.createElement("tr");
+                row.className = "reward-item";
+
+                row.appendChild(GenerateBuyButton(blockedSites[site]));
+
+                var td = document.createElement("td");
+                td.appendChild(document.createTextNode(blockedSites[site].hostname));
+                row.appendChild(td);
+
+                td = document.createElement("td");
+                var pencil = CreateOnClickLink(updateSiteCost, blockedSites[site]);
+                pencil.innerHTML = '<img src="img/pencil.png" />';
+                td.appendChild(pencil);
+                row.appendChild(td);
+
+                td = document.createElement("td");
+                var trash = CreateOnClickLink(removeSite, blockedSites[site]);
+                trash.innerHTML = '<img src="img/trash.png" />';
+                td.appendChild(trash);
+                row.appendChild(td);
+
+                table.appendChild(row);
+                var divider = document.createElement("tr");
+                divider.appendChild(document.createElement("td"));
+                table.appendChild(divider);
+
+
+            }
+        }
+        document.body.appendChild(table);
+        document.body.AppendBreak();
+        document.body.appendChild(GenerateCredentialFields());
+
+        var dosh = document.createElement("h1");
+        var coin = document.createElement("span");
+        coin.className = "shop_gold";
+        dosh.appendChild(coin);
+        dosh.AppendText(" : " + Vars.Monies.toFixed(2));
+        document.body.appendChild(dosh);
+    });
+});
+
+
+function GenerateCredentialFields() {
+    var div = document.createElement("div");
+    div.innerHTML =
+        '<div id="EmptyUID">If either field is empty, this extension does not work.</div>' +
+        '<label for="uid">HabitRPG User ID:</label><br /><input id="uid" type="text" value="' + Vars.UserData.Credentials.uid + '"/></br>' +
+        '<label for="apiToken">HabitRPG API Token:<br /></label><input id="apiToken" type="text" value="' + Vars.UserData.Credentials.apiToken + '"/></br>';
+    var button = document.createElement("button");
+    button.AppendText("Save");
+    button.onclick = updateCredentials;
+    button.type = "submit";
+    div.appendChild(button);
+    return div;
+}
+function CreateOnClickLink(onclick, param1,param2,param3) {
+    var a = document.createElement("a");
+    a.href = "#";
+    a.onclick = function () { onclick(param1, param2, param3); }
+    return a;
+}
+
+function GenerateBuyButton(site) {
+    var buyLink = CreateOnClickLink(chrome.tabs.create, { url: "http://" + site.hostname });
+    var buyButton = document.createElement("td");
+    buyButton.className = "gp";
+    var coin = document.createElement("span");
+    coin.className = "shop_gold";
+    buyButton.appendChild(coin);
+    buyButton.AppendBreak();
+    var cost = site.cost;
+    if (cost % 1 != 0) cost = cost.toFixed(2);
+    buyButton.AppendText(cost);
+    buyLink.appendChild(buyButton);
+    return buyLink;
+}
+function GenerateBlockCommand(currentSiteUrl) {
+    var hostname = new URL(currentSiteUrl).hostname;
+    var currentSite = Vars.UserData.GetBlockedSite(hostname);
+    var link = document.createElement("a");
+    if (currentSite) {
+        link.AppendText("Un-Block Site!");
+        link.onclick = function () {
+            Vars.UserData.RemoveBlockedSite(hostname);
+            SaveUserSettings();
+        }
+    } else {
+        link.AppendText("Block Site!");
+        link.onclick = function () {
+            currentSite = Vars.UserData.AddBlockedSite(hostname, 0, Date.now());
+            updateSiteCost(currentSite);
+            SaveUserSettings();
+        }
+    }
+    link.href = "#";
+    document.body.appendChild(link);
+}
+function SaveUserSettings() {
+    window.close();
+}
+
+function updateSiteCost(site) {
+        var selection = parseFloat(window.prompt("Enter a cost", site.cost));
+        if (!isNaN(selection) && selection >= 0) {
+            site.cost = selection;
+            SaveUserSettings();
+        }
+}
+function removeSite(site) {
+    return function () {
+        if (confirm("Are you sure you want to remove this block?")) {
+            Vars.UserData.RemoveBlockedSite(site.hostname);
+            SaveUserSettings();
+        }
+    }
+}
+function updateCredentials() {
+    Vars.UserData.Credentials.uid = document.getElementById("uid").value;
+    Vars.UserData.Credentials.apiToken = document.getElementById("apiToken").value;
+    SaveUserSettings();
+    background.FetchHabitRPGData();
+    if (Vars.UserData.Credentials.uid == "3e595299-3d8a-4a10-bfe0-88f555e4aa0c") {
+        alert("I might have a small crush on you.");
+    }
+
+}
 function getCurrentTabUrl(callback) {
     var queryInfo = {
         active: true,
@@ -11,160 +157,4 @@ function getCurrentTabUrl(callback) {
         console.assert(typeof url == 'string', 'tab.url should be a string');
         callback(url);
     });
-}
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    
-    getCurrentTabUrl(function(url) {
-        var domain = new URL(url).hostname;
-        var index = -1;
-        for (var j = 0; j < background.Domains.length; j++) {
-            if (background.Domains[j].hostname == domain) {
-                index = j;
-                break;
-            }
-        }
-
-        var link = document.createElement("a");
-        var node;
-        if (index > -1) {
-            node = document.createTextNode("Un-Block Site!");
-            link.onclick = function() {
-                background.Domains.splice(index, 1);
-                window.close();
-                chrome.storage.sync.set({ 'save': background.Domains }, function () { });
-            }
-        } else {
-            node = document.createTextNode("Block Site!");
-            link.onclick = function () {
-                background.Domains.push(new background.Domain(domain, 0, Date.now()));
-                updateDomainCost(background.Domains.length - 1)();
-                window.close();
-                chrome.storage.sync.set({ 'save': background.Domains }, function () { });
-                
-            }
-
-        }
-        link.appendChild(node);
-        document.body.appendChild(link);
-        link.href = "#";
-
-
-        document.body.appendChild(document.createElement("br"));
-        var para = document.createElement("p");
-        para.appendChild(document.createTextNode("Blocked Sites:"));
-        document.body.appendChild(para);
-        var table = document.createElement("table");
-
-        for (var i = 0; i < background.Domains.length; i++) {
-            var tr = document.createElement("tr");
-            tr.className = "reward-item";
-            var buyLink = document.createElement("a");
-            buyLink.href = "#";
-            buyLink.onclick = openTab("http://"+background.Domains[i].hostname);
-            var buy = document.createElement("td");
-            buy.className = "gp";
-            var span = document.createElement("span");
-            span.className = "shop_gold";
-            buy.appendChild(span);
-            buy.appendChild(document.createElement("br"));
-            var cost = background.Domains[i].cost;
-            if (cost%1 != 0)cost = cost.toFixed(2);
-            buy.appendChild(document.createTextNode(cost));
-            buyLink.appendChild(buy);
-            tr.appendChild(buyLink);
-
-            var td = document.createElement("td");
-            td.appendChild(document.createTextNode(background.Domains[i].hostname));
-            tr.appendChild(td);
-
-            td = document.createElement("td");
-            var pencil = document.createElement("a");
-            pencil.appendChild(document.createTextNode("\u270F"));
-            pencil.onclick = updateDomainCost(i);
-            pencil.href = "#";
-            pencil.innerHTML = '<img src="img/pencil.png" />';
-            td.appendChild(pencil);
-            tr.appendChild(td);
-
-            td = document.createElement("td");
-            var trash = document.createElement("a");
-            trash.appendChild(document.createTextNode("\uE017"));
-            trash.onclick = removeDomain(i);
-            trash.href = "#";
-            trash.innerHTML = '<img src="img/trash.png" />';
-            td.appendChild(trash);
-            tr.appendChild(td);
-
-            table.appendChild(tr);
-            tr = document.createElement("tr");
-            tr.innerHTML = "<td></td>";
-            tr.style.height = 10;
-            table.appendChild(tr);
-
-
-        }
-        document.body.appendChild(table);
-
-        document.body.appendChild(document.createElement("br"));
-
-        var div = document.createElement("div");
-        div.innerHTML =
-            '<div id="EmptyUID">If either field is empty, this extension does not work.</div>' +
-            '<label for="uid">HabitRPG User ID:</label><br /><input id="uid" type="text" value="' + background.Credentials[0] + '"/></br>' +
-            '<label for="apiToken">HabitRPG API Token:<br /></label><input id="apiToken" type="text" value="' + background.Credentials[1] + '"/></br>';
-        var button = document.createElement("button");
-        button.appendChild(document.createTextNode("Save"));
-        button.onclick = updateCredentials;
-        button.type = "submit";
-        div.appendChild(button);
-        document.body.appendChild(div);
-
-        var p2 = document.createElement("h1");
-        var dosh = document.createElement("span");
-        dosh.className = "shop_gold";
-        p2.appendChild(dosh);
-        p2.appendChild(document.createTextNode(" : " + background.Monies.toFixed(2)));
-
-        document.body.appendChild(p2);
-    });
-});
-
-function openTab(oUrl) {
-    return function() {
-        chrome.tabs.create({ url: oUrl });
-    }
-}
-
-function updateDomainCost(domainNumber) {
-    return function() {
-        var selection;
-        do {
-            selection = parseFloat(window.prompt("Enter a cost", 5));
-        } while (isNaN(selection) || selection < 0);
-        background.Domains[domainNumber].cost = selection;
-        window.close();
-        chrome.storage.sync.set({ 'save': background.Domains }, function () { });
-    }
-}
-function removeDomain(index) {
-    return function () {
-        if (confirm("Are you sure you want to remove this block?")) {
-            background.Domains.splice(index, 1);
-            window.close();
-            chrome.storage.sync.set({ 'save': background.Domains }, function() {});
-        }
-    }
-}
-function updateCredentials() {
-    background.Credentials = [document.getElementById("uid").value, document.getElementById("apiToken").value];
-    chrome.storage.sync.set({ 'creds': background.Credentials }, function () { });
-    FetchUserData();
-    if (background.Credentials[0] == "3e595299-3d8a-4a10-bfe0-88f555e4aa0c") {
-        alert("I might have a small crush on you.");
-    }
-    window.close();
-
 }
